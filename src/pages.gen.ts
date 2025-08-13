5d583850c100e6638f9aeef52f2ae079e1ecd40a
Rob Marscher <rob@robmarscher.com>
2025-08-12 23:14:43 -0400

    Add route group example

--- src/pages.gen.ts
+++ src/pages.gen.ts
@@ -4,13 +4,16 @@
 import type { PathsForPages, GetConfigResponse } from 'waku/router';
 
 // prettier-ignore
-import type { getConfig as File_About_getConfig } from './pages/about';
+import type { getConfig as File_ProtectedAbout_getConfig } from './pages/(protected)/about';
+// prettier-ignore
+import type { getConfig as File_ProtectedSecret_getConfig } from './pages/(protected)/secret';
 // prettier-ignore
 import type { getConfig as File_Index_getConfig } from './pages/index';
 
 // prettier-ignore
 type Page =
-| ({ path: '/about' } & GetConfigResponse<typeof File_About_getConfig>)
+| ({ path: '/about' } & GetConfigResponse<typeof File_ProtectedAbout_getConfig>)
+| ({ path: '/secret' } & GetConfigResponse<typeof File_ProtectedSecret_getConfig>)
 | ({ path: '/' } & GetConfigResponse<typeof File_Index_getConfig>);
 
 // prettier-ignore
