31d2a0565ef8a408d79e7b5b41f7055d871f1fbd
Rob Marscher <rob@robmarscher.com>
2025-08-13 09:08:43 -0400

    Add comment with link to better-auth docs

--- src/auth.ts
+++ src/auth.ts
@@ -1,6 +1,8 @@
 import { betterAuth } from "better-auth";
 import Database from "better-sqlite3";
 
+// Other database adapters and options are available
+// https://www.better-auth.com/docs/installation#configure-database
 export const auth = betterAuth({
   database: new Database("./sqlite.db"),
   emailAndPassword: {
