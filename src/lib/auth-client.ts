f515b8a61fc566357bc0837ad6273fcdc5c7fe17
Rob Marscher <rob@robmarscher.com>
2025-08-13 09:18:47 -0400

    better-auth client url is optional

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,9 +1,5 @@
 import { createAuthClient } from "better-auth/react";
 
-export const authClient = createAuthClient({
-  /** The base URL of the server (optional if you're using the same domain) */
-  baseURL:
-    import.meta.env.WAKU_PUBLIC_BETTER_AUTH_URL || "http://localhost:3000",
-});
+export const authClient = createAuthClient();
 
 export type Session = typeof authClient.$Infer.Session;
